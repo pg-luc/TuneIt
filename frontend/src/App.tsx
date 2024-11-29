e59a055978b0ea124801b75f693f7ec87662f162
pg-luc <paolucindo@gmail.com>
2024-11-29 22:17:59 +0800

    still trying to figure out Tailwind css not working

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,9 @@
-
 function App() {
   return (
-    <>
-      <div>
-        <h1 className="text-red-500">
-          Hello world!
-        </h1>
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
-      </div>
-    </>
-  )
+    <div className="text-center p-4">
+      <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind!</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
