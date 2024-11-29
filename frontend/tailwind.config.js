e394bf06af4db89771783ae76ffd2760f75b3712
pg-luc <paolucindo@gmail.com>
2024-11-29 21:58:05 +0800

    Updated some tailwind configs

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,7 @@ export const content = [
   "./app/**/*.{js,ts,jsx,tsx,mdx}",
   "./pages/**/*.{js,ts,jsx,tsx,mdx}",
   "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  "./ index.html",
 
   // Or if using `src` directory:
   "./src/**/*.{js,ts,jsx,tsx,mdx}",
