f28ce4cc8eb64e3716fc201ad8954a1bdee4f142
pg-luc <paolucindo@gmail.com>
2024-12-01 15:27:44 +0800

    setting up routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,14 @@
 import express from "express";
-import 'dotenv/config';
+import dotenv from "dotenv";
 
+// Routes
+import users_route from "./routes/users.routes.js";
+
+dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
-app.get('/', (req, res) => {
-    res.send("TUNE IT HOMEPAGE");
-})
+app.use("/api/users", users_route);
 
 app.listen(port, () => {
     console.log(`Application is listening on PORT: ${port}`);
